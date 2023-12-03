7df3a022d0e4d69dd57dd6eb0c6e29235515a32d
Allan Chua <allanchua.officefiles@gmail.com>
2023-12-04 05:58:49 +0800

    Remove console.log statement from CLI

--- cli/helpers/get-account-ddbs-with-desc.mjs
+++ cli/helpers/get-account-ddbs-with-desc.mjs
@@ -67,16 +67,11 @@ export async function getAllDynamoDBTablesWithDesc(profileName) {
         const descCmd = new DescribeTableCommand({ TableName: rt });
         const response = await ddbClient.send(descCmd);
 
-        console.log(response?.Table?.SSEDescription);
-        const hasEncryption =
-          response?.Table?.SSEDescription?.Status === "ENABLED" || false;
-
         tables.push({
           name: rt,
           sana: {
             region,
             table: response.Table,
-            hasEncryption,
           },
         });
 
